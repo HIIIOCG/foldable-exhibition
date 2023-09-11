71d0f767e2916d9e8914bf2e586c7519b1e49c4c
Hans <chiuhans111@gmail.com>
2023-09-11 22:57:19 +0800

    POC

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.glb$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'assets/models/[name].[hash:7].[ext]',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
 })
